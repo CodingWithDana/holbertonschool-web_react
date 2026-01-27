bc3468e6018607a9ee3fc5e5496ba7e0ce672ebc
Dana Le <danadaole@gmail.com>
2026-01-27 23:54:29 +1100

    read-only properties & dynamic properties

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -1,51 +1,9 @@
-interface Student {
-    firstName: string,
-    lastName: string,
-    age: number,
-    location: string,
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsofExperience?: number;
+    location: string;
+    // allow any additional property
+    [key: string]: any;
 }
-
-const student1: Student = {
-    firstName: "Dana",
-    lastName: "L",
-    age: 18,
-    location: "Melbourne",
-}
-
-const student2: Student = {
-    firstName: "Yess",
-    lastName: "V",
-    age: 25,
-    location: "Sydney",
-}
-
-const studentList: Student[] = [student1, student2];
-
-function createTable(studentList: Student[]) {
-    let table = document.createElement('table');
-    let thead = document.createElement('thead');
-    let tbody = document.createElement('tbody');
-    let headerRow = document.createElement('tr');
-
-    const headers: string[] = ['firstName', 'location'];
-    headers.forEach(headerText => {
-        let header = document.createElement('th');
-        header.textContent = headerText;
-        headerRow.appendChild(header);
-    });
-
-    table.appendChild(thead);
-    thead.appendChild(headerRow);
-    table.appendChild(tbody);
-
-    studentList.forEach(student => {
-        let row = tbody.insertRow();
-        let firstNameCell = row.insertCell(0);
-        firstNameCell.textContent = student.firstName;
-        let locationCell = row.insertCell(1);
-        locationCell.textContent = student.location;
-    });
-
-    document.body.appendChild(table);
-}
-createTable(studentList);
---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,51 @@
+interface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string,
+}
+
+const student1: Student = {
+    firstName: "Dana",
+    lastName: "L",
+    age: 18,
+    location: "Melbourne",
+}
+
+const student2: Student = {
+    firstName: "Yess",
+    lastName: "V",
+    age: 25,
+    location: "Sydney",
+}
+
+const studentList: Student[] = [student1, student2];
+
+function createTable(studentList: Student[]) {
+    let table = document.createElement('table');
+    let thead = document.createElement('thead');
+    let tbody = document.createElement('tbody');
+    let headerRow = document.createElement('tr');
+
+    const headers: string[] = ['firstName', 'location'];
+    headers.forEach(headerText => {
+        let header = document.createElement('th');
+        header.textContent = headerText;
+        headerRow.appendChild(header);
+    });
+
+    table.appendChild(thead);
+    thead.appendChild(headerRow);
+    table.appendChild(tbody);
+
+    studentList.forEach(student => {
+        let row = tbody.insertRow();
+        let firstNameCell = row.insertCell(0);
+        firstNameCell.textContent = student.firstName;
+        let locationCell = row.insertCell(1);
+        locationCell.textContent = student.location;
+    });
+
+    document.body.appendChild(table);
+}
+createTable(studentList);
