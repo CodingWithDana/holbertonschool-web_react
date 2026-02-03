69201b788c4e315a63c3fd2ac662c07fcc4362ce
Dana Le <danadaole@gmail.com>
2026-02-03 14:11:05 +1100

    learn about string literal type

--- TypeScript/task_7/js/main.ts
+++ TypeScript/task_7/js/main.ts
@@ -1,54 +1,14 @@
-// Interfaces
-export interface DirectorInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
-}
-
-export interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
-
-// Classes
-export class Director implements DirectorInterface {
-  workFromHome(): string {
-    return 'Working from home';
-  }
-
-  getCoffeeBreak(): string {
-    return 'Getting a coffee break';
-  }
-
-  workDirectorTasks(): string {
-    return 'Getting to director tasks';
-  }
-}
-
-export class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return 'Cannot work from home';
-  }
-
-  getCoffeeBreak(): string {
-    return 'Cannot have a break';
-  }
-
-  workTeacherTasks(): string {
-    return 'Getting to work';
-  }
-}
+// String literal type
+type Subjects = 'Math' | 'History';
 
 // Function
-export function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number' && salary < 500) {
-    return new Teacher();
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
   }
-  return new Director();
+  return 'Teaching History';
 }
 
-// Expected result tests
-console.log(createEmployee(200));     // Teacher
-console.log(createEmployee(1000));    // Director
-console.log(createEmployee('$500'));  // Director
+// Expected result
+console.log(teachClass('Math'));     // Teaching Math
+console.log(teachClass('History'));  // Teaching History
