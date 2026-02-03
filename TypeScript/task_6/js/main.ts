d9db0c43654d71186019ff0c169b40f4870f5216
Dana Le <danadaole@gmail.com>
2026-02-03 14:08:40 +1100

    learn about type predicate

--- TypeScript/task_6/js/main.ts
+++ TypeScript/task_6/js/main.ts
@@ -48,7 +48,26 @@ export function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
+// Type predicate
+export function isDirector(
+  employee: Director | Teacher
+): employee is Director {
+  return employee instanceof Director;
+}
+
+// Function using type narrowing
+export function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
+
 // Expected result tests
 console.log(createEmployee(200));     // Teacher
 console.log(createEmployee(1000));    // Director
 console.log(createEmployee('$500'));  // Director
+
+// Expected result tests
+console.log(executeWork(createEmployee(200)));   // Getting to work
+console.log(executeWork(createEmployee(1000)));  // Getting to director tasks
